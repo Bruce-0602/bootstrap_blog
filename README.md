802c95d1b86ef86e35b5f43e4d46f4664adaa881
Bruce-0602 <liangjitian777@gmail.com>
2022-06-18 00:17:17 +1000

    added email functionality“

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Practise Blog
 Build by Bootstrap & Flask <br>
-Used npoint.io. customized API <br>
-Used Clean Blog Template from Start Bootstrap's website
+Used npoint.io. customized API for blogs<br>
+Used Clean Blog Template from Start Bootstrap's website <br>
+Send email using via Gmail app password for HTML form in Contact Me page 
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 import requests
-
+# import smtplib
+import os
 
 app = Flask(__name__)
+# mail = Mail(app)
 blog_api = "https://api.npoint.io/4780dc7ea12348799258"
 posts = requests.get(blog_api).json()
+OWN_EMAIL = os.environ['OWN_EMAIL']
+OWN_PASSWORD = os.environ['OWN_PASSWORD']
+APP_PASSWORD = os.environ['APP_PASSWORD']
+
+# set up gmail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = OWN_EMAIL
+app.config['MAIL_PASSWORD'] = APP_PASSWORD
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 @app.route("/")
@@ -17,11 +32,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = None
@@ -29,6 +39,26 @@ def show_post(post_id):
         if blog_post["id"] == post_id:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    msg = Message('New Form', sender=OWN_EMAIL, recipients=[OWN_EMAIL])
+    msg.body = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    mail.send(msg)
+    # older version
+    # with smtplib.SMTP("smtp.gmail.com") as connection:
+    #     connection.starttls()
+    #     connection.login(OWN_EMAIL, OWN_PASSWORD)
+    #     connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
